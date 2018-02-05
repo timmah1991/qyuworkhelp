0c204fdfdafd5f666620203e816635f4e93a4acd
Thomas Kittredge <thomaskittredge@Thomass-MBP.solderworks.com>
2018-02-04 21:55:44 -0700

    Added partial progress notices for ingesting extra large files

--- databaseprofiler.sh
+++ databaseprofiler.sh
@@ -4,6 +4,7 @@
 
         for i in `seq 1 $2`;
         do
+                echo "Starting Column #"$i
                 #cat $1 | awk -F "\"*,\"*" '{print $'$i'}' | grep -c '[A-Za-z]'
                 COLUMN_HEADER=$(cat $1 | awk -F "\"*,\"*" 'NR==1{print $'$i'}')
                 if [[ $(cat $1 | awk -F "\"*,\"*" '{print $'$i'}' | grep -c '[A-Za-z]') > "1" ]]; then
@@ -13,6 +14,7 @@
                     #echo "NUMBERIC ONLY CHARACTERS IN COLUMN #"$i" WITH COLUMN HEADER "$COLUMN_HEADER
                     echo $COLUMN_HEADER >> numeric.txt
                 fi
+                echo "Finished Column #"$i
         done  
 
         echo "-----------------------------"
