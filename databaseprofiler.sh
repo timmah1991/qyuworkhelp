78ec545ff0c7d389db33bd095c2aecaa5c995921
Thomas Kittredge <thomaskittredge@Thomass-MBP.solderworks.com>
2018-02-04 22:12:41 -0700

    Added the ability top send a max_row param (third param) to the database profiler

--- databaseprofiler.sh
+++ databaseprofiler.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-# Use like this: ./databaseprofiler.sh path/to/file.txt numberofrowstoanalyze
+# Use like this: ./databaseprofiler.sh path/to/file.txt numberofcolumnstoanalyze maxrows
 #TODO: add a quick bit up here to count the number of columns in CSV automatically
+#TODO: add a "max row sample count" parameter to help with performance  ~~ DONE
 
         for i in `seq 1 $2`;
         do
                 echo "Starting Column #"$i" out of "$2 
                 #cat $1 | awk -F "\"*,\"*" '{print $'$i'}' | grep -c '[A-Za-z]'
                 COLUMN_HEADER=$(cat $1 | awk -F "\"*,\"*" 'NR==1{print $'$i'}')
-                if [[ $(cat $1 | awk -F "\"*,\"*" '{print $'$i'}' | grep -c '[A-Za-z]') > "1" ]]; then
+                if [[ $(cat $1 | sed -n 1,$3p | awk -F "\"*,\"*" '{print $'$i'}' | grep -c '[A-Za-z]') > "1" ]]; then
                     #echo "NON-NUMERIC FOUND AT COLUMN #"$i" WITH COLUMN HEADER "$COLUMN_HEADER
                     echo $COLUMN_HEADER >> non_numeric.txt
                 else
