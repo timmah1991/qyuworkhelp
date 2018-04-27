c92df5b3ba10cbdcccc301b0f20d1601017047b2
Thomas Kittredge <thomaskittredge@Thomass-MBP-3.domain>
2018-04-26 21:04:30 -0600

    Added code to EOF

--- EOF.sh
+++ EOF.sh
@@ -2,5 +2,16 @@
 
 #Takes a directory path and, lists the subdirectories and performs a command for each directory in the path. 
 
+while getopts p:c:d: option
+do
+    case "${option}"
+    in
+    p) DIRPATH=${OPTARG};;
+    c) COMMANDTORUN=${OPTARG};;
+    d) DEBUG=${OPTARG};;
+    esac
+done
 
-
+for fp in `cd $DIRPATH & ls -d -1 $PWD/**`; do
+    echo "Executing $COMMANDTORUN With Parameter $fp"
+done
